53f0c6eea986e1d361826beb75465119651084e2
xsign <akm129@gmail.com>
2017-04-15 21:27:44 +0200

    Tests updated

--- src/BinPacking/Tests/UnitTest.java
+++ src/BinPacking/Tests/UnitTest.java
@@ -10,9 +10,10 @@ import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.*;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
 
 /**
  * Created by Xsignati on 21.03.2017.
@@ -21,36 +22,55 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class UnitTest {
     @Test
     public void test() {
-        //source list
+        //source lists
         ObservableList<Box> boxList = FXCollections.observableArrayList();
         ObservableList<Bin> binList = FXCollections.observableArrayList();
-        boxList.add(new Box(100,100,100));
-        boxList.add(new Box(100,100,100));
-        boxList.add(new Box(100,100,100));
-        boxList.add(new Box(100,100,100));
-
-        //target list
         ObservableList<Box> testBoxList = FXCollections.observableArrayList();
-        Box b1 = new Box(100,100,100);
-        b1.setCoordinates(0,0,0);
-        Box b2 = new Box(100,100,100);
-        b2.setCoordinates(100,0,0);
-        Box b3 = new Box(100,100,100);
-        b3.setCoordinates(200,0,0);
-        Box b4 = new Box(100,100,100);
-        b4.setCoordinates(300,0,0);
-        testBoxList.add(b1);
-        testBoxList.add(b2);
-        testBoxList.add(b3);
-        testBoxList.add(b4);
 
-        //run loader
-        InputData inputData = new InputData(1000,1000,1000, binList, PackingStrategyFactory.getPS("BestFit"), boxList);
-        Loader loader = new Loader();
-        loader.run(inputData);
+        double binLength;
+        double binWidth;
+        double binHeight;
+
+        //Read data from txt. First line = bin size. Other lines = Box [l w h x y z]
+        try {
+            FileInputStream fi = new FileInputStream("src/BinPacking/Tests/input.txt");
+            InputStreamReader isr = new InputStreamReader(fi);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line = br.readLine();
+
+            String[] values = line.split(" ");
+            binLength = Double.parseDouble(values[0]);
+            binWidth = Double.parseDouble(values[1]);
+            binHeight = Double.parseDouble(values[2]);
+
+            while((line = br.readLine()) != null){
+                values = line.split(" ");
+                double boxLength = Double.parseDouble(values[0]);
+                double boxWidth = Double.parseDouble(values[1]);
+                double boxHeight = Double.parseDouble(values[2]);
+                double boxX = Double.parseDouble(values[3]);
+                double boxY = Double.parseDouble(values[4]);
+                double boxZ = Double.parseDouble(values[5]);
+
+                boxList.add(new Box(boxLength, boxWidth, boxHeight));
+
+                Box testBox = new Box(boxLength, boxWidth, boxHeight);
+                testBox.setCoordinates(boxX, boxY, boxZ);
+                testBoxList.add(testBox);
+            }
+
+            InputData inputData = new InputData(binLength, binWidth,binHeight, binList, PackingStrategyFactory.getPS("BestFit"), boxList);
+            Loader loader = new Loader();
+            loader.run(inputData);
+
+
+        }
+        catch(FileNotFoundException e)
+            {e.printStackTrace();}
+        catch(IOException e){e.printStackTrace();}
+
 
         assertThat(boxList, is(testBoxList));
-
-
     }
 }
